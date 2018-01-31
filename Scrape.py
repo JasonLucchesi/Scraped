9865b20f3e0a9e4f9028a4965145eb7905145c2f
JasonLucchesi <iamjasonlucchesi@gmail.com>
2018-01-31 13:18:32 +0000

    pageScrape()
    New pageScrape() added.

--- Scrape.py
+++ Scrape.py
@@ -2,11 +2,15 @@ import logging
 import requests
 from bs4 import BeautifulSoup
 
-logging.basicConfig(filename='404.log', level=logging.INFO, format='%(message)s')
+#logging.basicConfig(filename='404.log', level=logging.INFO, format='%(message)s')
 
 # URL to check
 url = "http://www.redefinebdl.com/rbh-updates/customer-service-recognition-at-rbh-portfolio-hotels"
+
 result = requests.get(url)
+
+c = result.content
+soup = BeautifulSoup(c, 'html.parser')
 
 def pageAlive():
     # Check page is alive
@@ -16,11 +20,25 @@ def pageAlive():
         print("He's dead, Jim!")
         logging.warning('{} - {}'.format(result.status_code, URL))
 
-def pageContent():
+def pageScrape():
     #Pull HTML
-    c = result.content
-    soup = BeautifulSoup(c, 'html.parser')
-    print(soup.prettify())
+    #print(soup.prettify())
+
+    #Page URL
+    print(url)
+
+    #Title Tag
+    titleTag = soup.head.title
+    print(titleTag)
+
+    #Page Content
+    pageContent = soup.find('div', class_='item-page')
+    print(pageContent)
+
+    #Images on page
+    #Image Path
+
 
 pageAlive()
-pageContent()
+pageScrape()
+
